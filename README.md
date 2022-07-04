e1756ac45e7eb644a507ec5175a5e49d5372af9b
tsitsiflora <tsitsimunikwa97@gmail.com>
2022-07-04 11:12:37 +0200

    Endpoint to get all currencies

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # currency_converter
-A currency convertor
+A currency convertor API
+
+Prerequisites
+
+1. Get a free API key from `fixer.io`. Fixer is a site for real time foreign exchange rates. To obtain an API key you must create an account.
+2. 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import requests
+from ACCESS_KEY import YOUR_API_KEY
 
 app = FastAPI()
 
@@ -15,4 +17,9 @@ async def root():
 async def get_all():
     '''This function retrieves all the currencies that the 
         currency converter is capable of supporting.'''
+    url = "https://api.apilayer.com/exchangerates_data/symbols"
+    payload = {}
+    headers= {"apikey": YOUR_API_KEY}
+    response = requests.get( url, headers=headers, data = payload)
+    return {response.text, response.status_code}
     
