a667cb4b6c33a7d18003d9b53a41a43ece19a938
tsitsiflora <tsitsimunikwa97@gmail.com>
2022-07-05 16:32:58 +0200

    It works

--- main.py
+++ main.py
@@ -2,117 +2,73 @@ from decimal import Decimal, getcontext
 from logging import log
 from ACCESS_KEY import YOUR_API_KEY
 from fastapi import FastAPI
-from schemas import Payment
+from schemas import Convert
 import babel.numbers as bn
 import requests
 
 
-api = FastAPI()
+app = FastAPI()
 headers= {"apikey": YOUR_API_KEY}
 
 
-# def get_rate(sender_currency: str, receiver_currency:str) -> str:
-#     '''This function fetches the current rates from the 
-#         external service.'''
-
-#     result = requests.get(
-#         f"https://api.apilayer.com/exchangerates_data/latest?symbols={receiver_currency}&base={sender_currency}"
-#         , headers=headers)
-#     print('From print')
-#     return str(result.json().get("rates").get(receiver_currency))
-
-
-# def convert_amount(convert: Convert) -> int:
-#     '''This function calculates the receiver amount, which is 
-#         the resulting amount by multiplying the rate 
-#         with the sender amount.'''
-
-#     rate = get_rate(convert.sender_currency, convert.receiver_currency)
-#     getcontext().prec = 4
-#     result_amount = rate * convert.sender_amount
-#     return result_amount
-
-
-# def conversion(convert: Convert) -> Convert:
-#     convert.receiver_amount = convert_amount(convert)
-#     return convert
-
-
-# def formatted_conversion(convert: Convert) -> Convert:
-#     '''This function converts the amounts into human
-#         readable currencies.'''
-
-#     convert.sender_amount_formatted = bn.format_currency(
-#         convert.sender_amount, convert.sender_currency
-#     )
-#     convert.receiver_amount_formatted = bn.format_currency(
-#         convert.receiver_amount, convert.receiver_currency
-#     )
-#     return convert
-
-
-# # defining the index endpoint decorator
-# @app.get("/")
-# async def root():
-#     '''This function welcomes you to the 
-#         currency converter API.'''
-#     return {"message":"Hey there, welcome to the currency convertor API."}
-
-
-# # defining the endpoint decorator to get all currencies
-# @app.get("/all")
-# async def get_all():
-#     '''This function retrieves all the currencies that the 
-#         currency converter is capable of supporting.'''
-
-#     url = "https://api.apilayer.com/exchangerates_data/symbols"
-#     response = requests.get( url, headers=headers)
-#     return {response.text, response.status_code}
-
-
-# # defining the endpoint decorator to convert from one 
-# # currency to another
-# @app.post("/convert", response_model=Convert)
-# async def convert_currency(convert: Convert):
-#     return formatted_conversion(convert_amount(convert))
-
 def get_rate(sender_currency: str, receiver_currency: str) -> str:
+    '''This function fetches the current rates from the 
+         external service.'''
+
     result = requests.get(
         f"https://api.apilayer.com/exchangerates_data/latest?symbols={receiver_currency}&base={sender_currency}"
         , headers=headers)
-    # result = requests.get(
-    #     "https://api.ratesapi.io/api/latest?"
-    #     f"base={sender_currency}&symbols={receiver_currency}")
-
     return str(result.json().get('rates').get(receiver_currency))
 
 
-def calculate_receiver_amount(payment: Payment) -> Decimal:
-    rate = get_rate(payment.sender_currency, payment.receiver_currency)
+def calculate_receiver_amount(convert: Convert) -> Decimal:
+    '''This function calculates the receiver amount, which is 
+         the resulting amount by multiplying the rate 
+         with the sender amount.'''
+
+    rate = get_rate(convert.sender_currency, convert.receiver_currency)
     # we can set and alter decimal precision setting however we want
     # https://docs.python.org/3.7/library/decimal.html#module-decimal
-    getcontext().prec = 5
-    return Decimal(rate) * Decimal(payment.sender_amount)
+    getcontext().prec = 6
+    return Decimal(rate) * Decimal(convert.sender_amount)
 
 
-def payment_with_conversion(payment: Payment) -> Payment:
-    payment.receiver_amount = calculate_receiver_amount(payment)
-    return payment
+def payment_with_conversion(convert: Convert) -> Convert:
+    convert.receiver_amount = calculate_receiver_amount(convert)
+    return convert
 
 
-def formatted_payment(payment: Payment) -> Payment:
-    payment.sender_amount_formatted = bn.format_currency(
-        payment.sender_amount, payment.sender_currency)
-    payment.receiver_amount_formatted = bn.format_currency(
-        payment.receiver_amount, payment.receiver_currency)
-    return payment
+def formatted_conversion(convert: Convert) -> Convert:
+    '''This function converts the amounts into human
+         readable currencies.'''
+
+    convert.sender_amount_formatted = bn.format_currency(
+        convert.sender_amount, convert.sender_currency)
+    convert.receiver_amount_formatted = bn.format_currency(
+        convert.receiver_amount, convert.receiver_currency)
+    return convert
 
 
-@api.get('/health')
-def health():
-    return
+@app.get('/')
+def check_health():
+    '''This function welcomes you to the 
+        currency converter API.'''
+    return {"message":"Hey there, welcome to the currency convertor API."}
+
+   
+# defining the endpoint decorator to get all currencies
+@app.get("/all")
+async def get_all():
+    '''This function retrieves all the currencies that the 
+         currency converter is capable of supporting.'''
+
+    url = "https://api.apilayer.com/exchangerates_data/symbols"
+    response = requests.get( url, headers=headers)
+    return {response.text, response.status_code}
 
 
-@api.post('/payment', response_model=Payment, tags=['payment'])
-def create_payment(payment: Payment):
-    return formatted_payment(payment_with_conversion(payment))
+# defining the endpoint decorator to convert from one 
+# # currency to another
+@app.post('/convert', response_model=Convert)
+def create_payment(convert: Convert):
+    return formatted_conversion(payment_with_conversion(convert))
--- schemas.py
+++ schemas.py
@@ -1,50 +1,9 @@
 from decimal import Decimal
-from numbers import Number
 from typing import Optional
 
 import babel.numbers as bn
 from pydantic import BaseModel, validator,UUID4
 
-# CURRENCIES = bn.list_currencies()
-# CURRENCY_VALUE_ERROR = ValueError(
-#     "Invalid currency code submitted. "
-#     f"Value must be one of the following {', '.join(CURRENCIES)}.")
-
-# class Convert(BaseModel):
-#     sender_currency: str
-#     sender_amount: int
-#     sender_amount_formatted: Optional[str]
-#     receiver_currency: str
-#     receiver_amount: Optional[int]
-#     receiver_amount_formatted: Optional[str]
-
-
-#     @validator('sender_amount')
-#     def positive_sender_amount(cls: BaseModel, v: int) -> int:
-#         '''Validating the amount that is being entered by the user, 
-#             valid amount must be a positive number.'''
-
-#         if v <= 0:
-#             raise ValueError('Amount must be a positive number.')
-#         return v
-
-#     @validator('receiver_currency')
-#     def valid_receiver_currency(cls: BaseModel, v: str) -> str:
-#         '''This function checks if the received currency is amount the 
-#         list of accepted currencies.'''
-
-#         if v not in CURRENCIES:
-#             raise CURRENCY_VALUE_ERROR
-#         return v
-
-#     @validator('sender_currency')
-#     def valid_sender_currency(cls: BaseModel, v: str) -> str:
-#         '''This function checks if the received currency is amount the 
-#         list of accepted currencies.'''
-
-#         if v not in CURRENCIES:
-#             raise CURRENCY_VALUE_ERROR
-#         return v
 
 CURRENCIES = bn.list_currencies()
 CURRENCY_VALUE_ERROR = ValueError(
